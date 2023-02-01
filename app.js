88f53187853797ce93aa7442525c52b00b9f7292
Zack Phillips <phillips.zackarie@outlook.com>
2023-02-01 01:16:30 +0000

    MAJOR CODE CHANGE

--- app.js
+++ app.js
@@ -1,38 +1,120 @@
+// Layer 1
 const express = require("express");
-const fileUpload = require("express-fileupload");
+const fileUpload = require('express-fileupload');
 const app = express();
+app.use(express.json({limit: '5mb'}));
 const port = 8080;
+const http = require('http');
+const fs = require('fs');
 
-app.use(
-  fileUpload({
-    limits: {
-      fileSize: 10000000,
-    },
-    abortOnLimit: true,
-  })
-);
+// default options
+app.use(fileUpload());
 
-// Add this line to serve our index.html page
-app.use(express.static("public"));
+// Layer 1 display form and image links
+app.get('/', (req, res) => {
+    http.get('http://10.142.0.3:8080/images', (response) => {
+        const {statusCode} = response;
+        const contentType = response.headers['content-type'];
 
+        let error;
+        // Any 2xx status code signals a successful response but
+        // here we're only checking for 200.
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+        }
+
+        if (error) {
+            console.error(error.message);
+            // Consume response data to free up memory
+            response.resume();
+            return;
+        }
+
+        response.setEncoding('utf8');
+        let rawData = '';
+        response.on('data', (chunk) => {
+            rawData += chunk;
+        });
+        response.on('end', () => {
+            try {
+                // console.log(rawData);
+                let formHTML = `
+<form action="/upload" method="POST" enctype="multipart/form-data">
+    <input type="file" name="image">
+    <button type="submit">Upload</button><br /><br />
+    ${rawData}
+</form>
+`;
+                res.send(formHTML);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
+});
 
 app.post("/upload", (req, res) => {
-  // Get the file that was set to our field named "image"
-  const { image } = req.files;
+    const postData = JSON.stringify(req.files);
+    let options = {
+        host: '10.142.0.3', port: port, path: '/upload', method: 'POST', headers: {
+            'Content-Type': 'application/json', 'Content-Length': Buffer.byteLength(postData),
+        }
+    };
 
-  // If no image submitted, exit
-  if (!image) return res.sendStatus(400);
+    let httpRequest = http.request(options, function (response) {
+        console.log(`STATUS: ${response.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
+    });
 
-  // Move the uploaded image to our upload folder
-  image.mv(__dirname + "/files/" + image.name);
+    httpRequest.write(postData);
+    httpRequest.end();
+    res.redirect('/');
+});
 
-  // All good
- // res.sendStatus(200);
-  console.log(req.method);
-  console.log("Successful picture upload!");
+app.get("/images/*", (req, res) => {
+    let paramImage = req.params[0];
+    http.get('http://10.142.0.3:8080/images/' + paramImage, (result) => {
+        const {statusCode} = result;
+        const contentType = result.headers['content-type'];
+
+        let error;
+        // Any 2xx status code signals a successful response but
+        // here we're only checking for 200.
+        if (statusCode !== 200) {
+            error = new Error('Request Failed.\n' + `Status Code: ${statusCode}`);
+        }
+        if (error) {
+            console.error(error.message);
+            // Consume response data to free up memory
+            result.resume();
+            return;
+        }
+
+        result.setEncoding('utf8');
+        let rawData = '';
+        result.on('data', (chunk) => {
+            rawData += chunk;
+        });
+
+        result.on('end', () => {
+            try {
+                const parsedData = JSON.parse(rawData);
+                let imgBuff = Buffer.from(parsedData.data);
+                let strHTML = "<img src='data:image/jpeg;base64," + imgBuff.toString('base64') + "' />";
+                res.send(strHTML);
+            } catch (e) {
+                console.error(e.message);
+            }
+        });
+    }).on('error', (e) => {
+        console.error(`Got error: ${e.message}`);
+    });
+
 });
 
 app.listen(port, (err) => {
-  if (err) console.log(err);
-  console.log(`Listening on port ${port}`);
+    if (err) console.log(err);
+    console.log(`Listening on port ${port}`);
 });
